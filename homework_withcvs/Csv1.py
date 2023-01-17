cef79816a41071c217cad1efe0dae535a483d13d
Asus <md.dilyaramustafaeva@gmail.com>
2023-01-18 00:37:19 +0500

    bu yerda mahallalar kiritiladi va har bir mahalladagi insonlar ism yosh va joylashuvi bilan ro'yhatlanadi , ma'lumotlarni ko'rish ma'lumotlar qo'shish  , va yangi mahalla nomini qo'shish mumkin

--- homework_withcvs/Csv1.py
+++ homework_withcvs/Csv1.py
@@ -1,43 +1,40 @@
 # work wth csv
 import csv
 
-distructs = open("distructsname.txt", 'r+')
 fieldnames = ['location', 'name', 'age']
-for i in range(3):
-    big_q = input("""
+
+big_q = input("""
      #1 = add new district
      #2 = add new data 
      #3 = know someone's location
      #4 = see all data
      >:""")
-    if big_q == '1':
-        new_district = input("enter new district name")
-        new_district1 = open(new_district, "w")
-        distructs.write(f"{new_district}")
-        distructs.write(",")
+if big_q == '1':
+    new_district = input("enter n ew district name")
+    new_district1 = open(new_district, "a+")
+    with open("distructsname.txt", "r+") as distructs:
+        distructs.write(f"{new_district} ,")
         print('Good')
-    elif big_q == '2':
-        add_distructs = []
+        csv.DictWriter(new_district, fieldnames, delimiter=',')
+elif big_q == '2':
+    add_distructs = []
+    with open("distructsname.txt", "r+") as distructs:
         a = distructs.read()
         add_distructs.append(a)
         ch_dis = input(f"{add_distructs} which distruct  :")
         try:
-            ch_dis1 = open(ch_dis, "r+")
+            ch_dis1 = open(ch_dis, "a+")
         except Exception:
             print('This data not found')
         else:
-            list_da = {
-                'name': input("enter name"),
-                'age': input("enter age"),
-                'location': input("enter location")
-            }
-            new_d = csv.DictWriter(ch_dis1, fieldnames, delimiter=',')
-            new_d.writeheader()
-            new_d.writerow(list_da)
+            location: input("enter location")
+            name: input("enter name")
+            age: input("enter age")
+            csv.writer(a)
 
-
-    elif big_q == '3':
-        add_distructs = []
+elif big_q == '3':
+    add_distructs = []
+    with open("distructsname.txt", "r+") as distructs:
         a = distructs.read()
         add_distructs.append(a)
         ch_dis = input(f"{add_distructs} which distruct  :")
@@ -49,9 +46,10 @@ for i in range(3):
             redoo = csv.DictReader(ch_dis1)
             print('name and location')
             for i in redoo:
-                print(f"{i['name']} = {i['location']}")
-    elif big_q == '4':
-        add_distructs = []
+            print(f"{i['name']} = {i['location']}")
+elif big_q == '4':
+    add_distructs = []
+    with open("distructsname.txt", "r+") as distructs:
         a = distructs.read()
         add_distructs.append(a)
         ch_dis = input(f"{add_distructs} which distruct  :")
@@ -62,4 +60,4 @@ for i in range(3):
         else:
             redoo = csv.DictReader(ch_dis1)
             for i in redoo:
-                print(i)
+            print(i)
