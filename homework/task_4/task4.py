fe429744ade6439070a89a38dc80e2d41a2e3970
Asus <md.dilyaramustafaeva@gmail.com>
2023-01-29 01:37:52 +0500

    homework

--- homework/task_4/task4.py
+++ homework/task_4/task4.py
@@ -1,6 +1,12 @@
 import csv
+
 with open("new_country_of_the_world.csv", "a+") as new_world:
     with open("country_of_the_world.csv", "r+") as old_world:
-        red = csv.DictReader(old_world)
-        for i in red:
-            new_b = new_world.write(i[''])
+        def get_v():
+            red = old_world.readlines()
+            for i in red:
+                i = i.replace(",", ".")
+                new_world.write(i)
+
+
+        get_v()
